a5a07ecb527f3e79f49b7b557a61a183b7dcdab0
sabrinali <sli7236@bths.edu>
2019-01-23 23:17:11 -0500

    a

--- src/com/company/SkyView.java
+++ src/com/company/SkyView.java
@@ -4,13 +4,13 @@ public class SkyView {
     private double[][] view;
     public SkyView(int numRows, int numCols, double[] scanned)
     {
-        double[][] view = new double[numRows][numCols];
+        view = new double[numRows][numCols];
         int scannedIndex = 0;
         for (int r = 0; r < numRows; r++)
         {
             if (r%2 == 0)
             {
-                for (int c = 0; c < numCols-1; c++)
+                for (int c = 0; c < numCols; c++)
                 {
                     view[r][c] = scanned[scannedIndex];
                     scannedIndex++;
@@ -18,7 +18,7 @@ public class SkyView {
             }
             else
             {
-                for (int c = numCols - 1; c > -1; c--)
+                for (int c = numCols - 1; c >= 0; c--)
                 {
                     view[r][c] = scanned[scannedIndex];
                     scannedIndex++;
@@ -29,17 +29,29 @@ public class SkyView {
 
     public double getAverage(int startRow, int endRow, int startCol, int endCol)
     {
-        int sum = 0;
+        double avg = 0;
         int count = 0;
         for (int r = startRow; r < endRow; r++)
         {
             for (int c = startCol; c < endCol; c++)
             {
-                sum += view[r][c];
+                avg += view[r][c];
                 count++;
             }
         }
-        int avg = sum/count;
+        avg /= count;
         return avg;
     }
+
+    public String toString(){
+        String str = "";
+        for(double[] row : view){
+            for(double column : row){
+                str += column + " ";
+            }
+            str+= "\n";
+        }
+        return str;
+    }
+
 }
